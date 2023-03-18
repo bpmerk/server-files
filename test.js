5653f71c7d26b58740832245afd8a91383ee1ba9
bpmerk <46238224+bpmerk@users.noreply.github.com>
2023-03-17 22:07:02 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,33 +1,56 @@
-export function addPinMessageButton() {
-  // Check if the revolt.chat object exists
-  if (!window.revolt || !window.revolt.chat) {
-    console.log('Error: revolt.chat object not found.');
-    return;
-  }
+const Revolt = require('revolt.js');
 
-  // Create a new button element for the Pin Message option
-  const pinButton = document.createElement('button');
+// Initialize Revolt client with your bot token
+const client = new Revolt.Client({
+    token: 'YOUR_BOT_TOKEN'
+});
 
-  // Set the button's innerHTML to an icon or text that represents the Pin Message option
-  pinButton.innerHTML = 'Pin Message';
+// Event listener for when the bot connects to Revolt server
+client.on('connected', () => {
+    console.log(`Logged in as ${client.user.username}!`);
+});
 
-  // Add an event listener to the button that will pin the selected message when clicked
-  pinButton.addEventListener('click', () => {
-    // Code to pin the selected message goes here
-    console.log('Selected message pinned.');
-  });
+// Event listener for when a message is received
+client.on('message', async (message) => {
+    // Check if the message was sent by the bot itself
+    if (message.author.bot) return;
 
-  // Use the revolt.chat API to add the button to the chat interface
-  const pinWidget = window.revolt.chat.createWidget({
-    onRender: () => {
-      // Get the container element for the message actions buttons
-      const container = document.querySelector('.message-actions');
+    // Check if the message contains the command to pin a message
+    if (message.content.startsWith('!pin')) {
+        // Check if the user has permission to pin messages
+        if (!message.member.permissions.has('pin_message')) {
+            await client.channels.sendMessage(message.channel_id, 'You don\'t have permission to pin messages!');
+            return;
+        }
 
-      // Insert the Pin Message button at the end of the container
-      container.appendChild(pinButton);
+        // Get the ID of the message to pin
+        const messageId = message.content.split(' ')[1];
+
+        // Get the channel object
+        const channel = await client.channels.fetch(message.channel_id);
+
+        // Get the message object
+        const messageToPin = await channel.messages.fetch(messageId);
+
+        // Check if the message is already pinned
+        if (messageToPin.isPinned) {
+            await client.channels.sendMessage(message.channel_id, 'That message is already pinned!');
+            return;
+        }
+
+        // Pin the message
+        await messageToPin.pin();
+
+        // Send a confirmation message
+        await client.channels.sendMessage(message.channel_id, `Pinned message: ${messageToPin.content}`);
     }
-  });
 
-  // Add the widget to the chat interface using the revolt.chat API
-  window.revolt.chat.addPluginWidget(pinWidget);
-}
+    // Check if the message was sent in a channel where a message was just pinned
+    if (message.channel.last_pin_timestamp && message.channel.last_pin_timestamp.getTime() === message.timestamp.getTime()) {
+        // React to the pinned message with a pin emoji
+        await message.react('📌');
+    }
+});
+
+// Log in to the Revolt server with your bot token
+client.login();
