9834c354b8d1d10826327a84426c0b5be522aeb1
bpmerk <46238224+bpmerk@users.noreply.github.com>
2023-03-17 22:13:32 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
-const Revolt = require('revolt.js');
+const { Client } = require('@revolt/api');
 
 // Initialize Revolt client with your bot token
-const client = new Revolt.Client({
-    token: 'YOUR_BOT_TOKEN'
+const client = new Client({
+    token: 'aJen-eI-xU4Klyt7wfcqAb5lz2xg6ke6kAOx1Fl4TZICHRTNnDo6aYmJraDHtcz-'
 });
 
 // Event listener for when the bot connects to Revolt server
-client.on('connected', () => {
+client.on('ready', () => {
     console.log(`Logged in as ${client.user.username}!`);
 });
 
@@ -19,22 +19,19 @@ client.on('message', async (message) => {
     if (message.content.startsWith('!pin')) {
         // Check if the user has permission to pin messages
         if (!message.member.permissions.has('pin_message')) {
-            await client.channels.sendMessage(message.channel_id, 'You don\'t have permission to pin messages!');
+            await message.channel.sendMessage('You don\'t have permission to pin messages!');
             return;
         }
 
         // Get the ID of the message to pin
         const messageId = message.content.split(' ')[1];
 
-        // Get the channel object
-        const channel = await client.channels.fetch(message.channel_id);
-
         // Get the message object
-        const messageToPin = await channel.messages.fetch(messageId);
+        const messageToPin = await client.channels.messages.fetch(message.channel_id, messageId);
 
         // Check if the message is already pinned
-        if (messageToPin.isPinned) {
-            await client.channels.sendMessage(message.channel_id, 'That message is already pinned!');
+        if (messageToPin.pinned) {
+            await message.channel.sendMessage('That message is already pinned!');
             return;
         }
 
@@ -42,15 +39,16 @@ client.on('message', async (message) => {
         await messageToPin.pin();
 
         // Send a confirmation message
-        await client.channels.sendMessage(message.channel_id, `Pinned message: ${messageToPin.content}`);
+        await message.channel.sendMessage(`Pinned message: ${messageToPin.content}`);
     }
 
     // Check if the message was sent in a channel where a message was just pinned
-    if (message.channel.last_pin_timestamp && message.channel.last_pin_timestamp.getTime() === message.timestamp.getTime()) {
+    const lastPinnedMessage = await message.channel.fetchLastPinTimestamp();
+    if (lastPinnedMessage && lastPinnedMessage.getTime() === message.timestamp.getTime()) {
         // React to the pinned message with a pin emoji
-        await message.react('📌');
+        await message.addReaction('📌');
     }
 });
 
 // Log in to the Revolt server with your bot token
-client.login();
+client.connect();
